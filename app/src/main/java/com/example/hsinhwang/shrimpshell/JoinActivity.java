89451e33452eed101a3dbebcf423d7e116947a21
UnaCP102 <xu3bji4vup326@gmail.com>
2018-08-28 17:10:11 +0800

    在Join頁面，增加生日Date Picker 的 code

--- app/src/main/java/com/example/hsinhwang/shrimpshell/JoinActivity.java
+++ app/src/main/java/com/example/hsinhwang/shrimpshell/JoinActivity.java
@@ -1,12 +1,91 @@
 package com.example.hsinhwang.shrimpshell;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class JoinActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class JoinActivity extends AppCompatActivity implements View.OnClickListener, DatePicker.OnDateChangedListener {
+    private int year, month, day;
+    private LinearLayout llDate;
+    private TextView tvDate;
+    private StringBuffer date;
+    private Context context;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_join);
+
+        context = this;
+        date = new StringBuffer();
+        handleView();
+        initDateTime();
+
+    }
+
+    private void handleView() {
+        llDate = (LinearLayout) findViewById(R.id.llDate);
+        tvDate = (TextView) findViewById(R.id.tvDate);
+        llDate.setOnClickListener(this);
+    }
+
+    private void initDateTime() {
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH) + 1;
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.llDate:
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setPositiveButton("設置", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (date.length() > 0) {
+                            date.delete(0, date.length());
+                        }
+                        tvDate.setText(date.append(String.valueOf(year)).append("年").append(String.valueOf(month)).append("月").append(day).append("日"));
+                        dialog.dismiss();
+                    }
+                });
+                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                final AlertDialog dialog = builder.create();
+                View dialogView = View.inflate(context, R.layout.dialog_date, null);
+                final DatePicker datePicker = dialogView.findViewById(R.id.datePicker);
+
+                dialog.setTitle("設置日期");
+                dialog.setView(dialogView);
+                dialog.show();
+                datePicker.init(year, month - 1, day, this);
+        }
+    }
+
+    @Override
+    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        this.year = year;
+        this.month = monthOfYear;
+        this.day = dayOfMonth;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        
     }
 }
