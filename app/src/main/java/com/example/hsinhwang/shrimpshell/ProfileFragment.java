f6de276c02377d81594b087891160b0ea3205409
UnaCP102 <xu3bji4vup326@gmail.com>
2018-08-28 20:33:45 +0800

    toEmployeePage遮罩

--- app/src/main/java/com/example/hsinhwang/shrimpshell/ProfileFragment.java
+++ app/src/main/java/com/example/hsinhwang/shrimpshell/ProfileFragment.java
@@ -25,7 +25,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        btn = view.findViewById(R.id.toEmployeePage);
+//        btn = view.findViewById(R.id.toEmployeePage);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
