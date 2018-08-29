40b2683cbeae9cdff5ede574edd526ae7c50c61a
UnaCP102 <xu3bji4vup326@gmail.com>
2018-08-29 19:41:51 +0800

    新增ProfileComment頁面code

--- app/src/main/java/com/example/hsinhwang/shrimpshell/ProfileCommentFragment.java
+++ app/src/main/java/com/example/hsinhwang/shrimpshell/ProfileCommentFragment.java
@@ -5,8 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
-public class ProfileCommentFragment extends Fragment{
+public class ProfileCommentFragment extends Fragment {
+    private Button btCommentCancel, btCommentOK;
+    private EditText etCommentText;
+    private RatingBar ratingBar;
+
+
     public ProfileCommentFragment(){
 
     }
@@ -14,11 +23,45 @@ public class ProfileCommentFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile_comment, container, false);
+        return inflater.inflate(R.layout.fragment_profile_comment, parent, false);
     }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        btCommentCancel = (Button) getActivity().findViewById(R.id.btCommentCancel);
+//        btCommentOK = (Button) getActivity().findViewById(R.id.btCommentOK);
+        etCommentText = (EditText) getActivity().findViewById(R.id.etCommentText);
+        ratingBar = (RatingBar) getActivity().findViewById(R.id.ratingBar);
+        ratingBar.setOnRatingBarChangeListener(ratingBarOnRatingBarChange);
+
+        btCommentCancel.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+                etCommentText.setText("");
+                Toast.makeText(getActivity(), "取消", Toast.LENGTH_LONG).show();
+            }
+
+        });
+        
+    }
+    public RatingBar.OnRatingBarChangeListener ratingBarOnRatingBarChange
+            = new RatingBar.OnRatingBarChangeListener()  {
+        @Override
+        public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+            Toast.makeText(getActivity(), "rating: " + rating, Toast.LENGTH_LONG).show();
+        }
+
+    };
+
 }
+
