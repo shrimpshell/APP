b685223b7bf5c66e5b4b2ebef81cc463a414a59a
黃信 <tedhwang8@hotmail.com>
2018-08-31 23:49:26 +0800

    活動頁日期
    1. 日期格式修改
    2. 活動頁可以scroll
    3. 拿掉測試Button

--- app/src/main/java/com/example/hsinhwang/shrimpshell/ReservedFragment.java
+++ app/src/main/java/com/example/hsinhwang/shrimpshell/ReservedFragment.java
@@ -1,18 +1,14 @@
 package com.example.hsinhwang.shrimpshell;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
-import com.example.hsinhwang.shrimpshell.ManagerPanel.ManagerHomeActivity;
 
 
 public class ReservedFragment extends Fragment {
-    private Button btn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,14 +19,6 @@ public class ReservedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_reserved, container, false);
-        btn = view.findViewById(R.id.toManagerPage);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), ManagerHomeActivity.class);
-                startActivity(intent);
-            }
-        });
         return view;
     }
 
