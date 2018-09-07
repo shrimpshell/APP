11d4d688e7c976a3149ff9c2e2e752524a82ad78
黃信 <tedhwang8@hotmail.com>
2018-09-07 21:47:36 +0800

    小修改

--- app/src/main/java/com/example/hsinhwang/shrimpshell/EmployeePanel/EmployeeHomeActivity.java
+++ app/src/main/java/com/example/hsinhwang/shrimpshell/EmployeePanel/EmployeeHomeActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.example.hsinhwang.shrimpshell.ManagerPanel.ManagerHomeActivity;
 import com.example.hsinhwang.shrimpshell.R;
