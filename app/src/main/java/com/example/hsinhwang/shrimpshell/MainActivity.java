3666e80b5bee791b5e8f358f6a9ac681a39c5877
黃信 <tedhwang8@hotmail.com>
2018-09-07 23:16:54 +0800

    修改部分如下 1. Manifest Activity 2. 新增dialog fragment 3. dialog 中/英文 4. 員工首頁背景圖

--- app/src/main/java/com/example/hsinhwang/shrimpshell/MainActivity.java
+++ app/src/main/java/com/example/hsinhwang/shrimpshell/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.hsinhwang.shrimpshell;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.Window;
 
@@ -20,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
     private Window window;
     boolean login = false;
     BottomNavigationView navigation;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+        if (keyCode == KeyEvent.KEYCODE_BACK && count == 0) {
+            new AlertDialogFragment().show(getSupportFragmentManager(), "exit");
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -96,6 +112,33 @@ public class MainActivity extends AppCompatActivity {
                 fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.content, fragment);
         fragmentTransaction.commit();
+    }
+
+    public static class AlertDialogFragment extends DialogFragment implements DialogInterface.OnClickListener {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.title_exit)
+                    .setMessage(R.string.confirm_exit)
+                    .setPositiveButton(R.string.confirm, this)
+                    .setNegativeButton(R.string.cancel, this)
+                    .create();
+        }
+
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            switch (i) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    if (getActivity() != null) {
+                        getActivity().finish();
+                    }
+                    break;
+                default:
+                    dialogInterface.cancel();
+                    break;
+            }
+        }
     }
 
 }
