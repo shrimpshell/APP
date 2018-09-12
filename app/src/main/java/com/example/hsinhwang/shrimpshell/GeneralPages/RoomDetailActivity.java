07709c25909887cc475462b67dde700aad737d1d
黃信 <tedhwang8@hotmail.com>
2018-09-13 02:04:33 +0800

    最新活動版本 1. home fragment的event跟room預覽 2. 主管頁面可以新/修/刪/讀

--- app/src/main/java/com/example/hsinhwang/shrimpshell/GeneralPages/RoomDetailActivity.java
+++ app/src/main/java/com/example/hsinhwang/shrimpshell/GeneralPages/RoomDetailActivity.java
@@ -1,17 +1,27 @@
 package com.example.hsinhwang.shrimpshell.GeneralPages;
 
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.hsinhwang.shrimpshell.Classes.Common;
+import com.example.hsinhwang.shrimpshell.Classes.ImageTask;
+import com.example.hsinhwang.shrimpshell.Classes.Rooms;
 import com.example.hsinhwang.shrimpshell.R;
 
+import java.io.ByteArrayOutputStream;
+
 public class RoomDetailActivity extends AppCompatActivity {
+    private final static String TAG = "RoomDetailActivity";
     private Window window;
     private TextView textView;
+    private ImageView roomImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +36,40 @@ public class RoomDetailActivity extends AppCompatActivity {
             window = getWindow();
             window.setStatusBarColor(Color.parseColor("#01728B"));
         }
+        roomImageView = findViewById(R.id.roomImageView);
         textView = findViewById(R.id.roomPageTextView);
     }
 
     private void handleBundle() {
         Bundle bundle = getIntent().getExtras();
         String text = "Data error!!";
+        int id = 0;
         if (bundle != null) {
             Object score = bundle.getSerializable("room");
+            Rooms room = (Rooms) score;
             if (score != null) {
-                text = score.toString();
+                id = room.getId();
+                text = room.getDetail();
             }
         }
+        loadRoomImage(id);
         textView.setText(text);
+    }
+
+    private void loadRoomImage(int id) {
+        String url = Common.URL + "/RoomServlet";
+        int imageSize = getResources().getDisplayMetrics().widthPixels / 3;
+        Bitmap bitmap = null;
+
+        try {
+            bitmap = new ImageTask(url, id, imageSize).execute().get();
+        } catch (Exception e) {
+            Log.e(TAG, e.toString());
+        }
+        if (bitmap != null) {
+            roomImageView.setImageBitmap(bitmap);
+        } else {
+            roomImageView.setImageResource(R.drawable.room_review);
+        }
     }
 }
--- app/src/main/java/com/example/hsinhwang/shrimpshell/ManagerPanel/AddEventActivity.java
+++ app/src/main/java/com/example/hsinhwang/shrimpshell/ManagerPanel/AddEventActivity.java
@@ -1,18 +1,47 @@
 package com.example.hsinhwang.shrimpshell.ManagerPanel;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.example.hsinhwang.shrimpshell.Classes.Common;
+import com.example.hsinhwang.shrimpshell.Classes.CommonTask;
+import com.example.hsinhwang.shrimpshell.Classes.Events;
+import com.example.hsinhwang.shrimpshell.Classes.Rooms;
 import com.example.hsinhwang.shrimpshell.R;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.io.ByteArrayOutputStream;
+import java.lang.reflect.Type;
+import java.sql.Date;
 
 public class AddEventActivity extends AppCompatActivity {
+    private final static String TAG = "AddEventActivity";
     private EditText etAddEventName, etAddEventDescription;
     private DatePicker etAddEventStartTime, etAddEventEndTime;
     private Button btnAddEvent;
+    private ImageView ivEvent;
+    private byte[] image;
+    private static final int REQUEST_PICK_PICTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +50,92 @@ public class AddEventActivity extends AppCompatActivity {
         initialization();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (resultCode == RESULT_OK) {
+            Uri uri = intent.getData();
+            int newSize = 512;
+            if (uri != null) {
+                String[] columns = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver().query(uri, columns,
+                        null, null, null);
+                if (cursor != null && cursor.moveToFirst()) {
+                    String imagePath = cursor.getString(0);
+                    cursor.close();
+                    Bitmap srcImage = BitmapFactory.decodeFile(imagePath);
+                    Bitmap downsizedImage = Common.downSize(srcImage, newSize);
+                    ivEvent.setImageBitmap(downsizedImage);
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    srcImage.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                    image = out.toByteArray();
+                }
+            }
+        }
+    }
+
     public void initialization() {
+        ivEvent = findViewById(R.id.ivEvent);
         etAddEventName = findViewById(R.id.etAddEventName);
         etAddEventDescription = findViewById(R.id.etAddEventDescription);
         etAddEventStartTime = findViewById(R.id.etAddEventStartTime);
         etAddEventEndTime = findViewById(R.id.etAddEventEndTime);
+
         btnAddEvent = findViewById(R.id.btnAddEvent);
         btnAddEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 存資料庫
+                insertAction();
                 finish();
             }
         });
+        ivEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, REQUEST_PICK_PICTURE);
+            }
+        });
     }
+
+    private void insertAction() {
+        String name = etAddEventName.getText().toString().trim(),
+                description = etAddEventDescription.getText().toString().trim();
+        String start = etAddEventStartTime.getYear() + "-" + (etAddEventStartTime.getMonth() + 1 > 10 ? etAddEventStartTime.getMonth() + 1 : "0" + (etAddEventStartTime.getMonth() + 1)) + "-" + etAddEventStartTime.getDayOfMonth(),
+                end = etAddEventEndTime.getYear() + "-" + (etAddEventEndTime.getMonth() + 1 > 10 ? etAddEventEndTime.getMonth() + 1 : "0" + (etAddEventEndTime.getMonth() + 1)) + "-" + etAddEventEndTime.getDayOfMonth();
+        if (Common.networkConnected(this)) {
+            String url = Common.URL + "/EventServlet";
+            Events event = new Events(0, name, description, start, end);
+            String imageBase64 = "";
+            if (image != null) imageBase64 = Base64.encodeToString(image, Base64.DEFAULT);
+            Log.e(TAG, new Gson().toJson(event));
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("action", "eventInsert");
+            jsonObject.addProperty("event", new Gson().toJson(event));
+            jsonObject.addProperty("imageBase64", imageBase64);
+            int count = 0;
+            try {
+                String result = new CommonTask(url, jsonObject.toString()).execute().get();
+                count = Integer.valueOf(result);
+            } catch (Exception e) {
+//                Log.e(TAG, e.toString());
+            }
+            if (count == 0) {
+                Common.showToast(this, R.string.msg_InsertFail);
+            } else {
+                Common.showToast(this, R.string.msg_InsertSuccess);
+            }
+        } else {
+            Common.showToast(this, R.string.msg_NoNetwork);
+        }
+    }
+
+    JsonSerializer<Date> ser = new JsonSerializer<Date>() {
+        @Override
+        public JsonElement serialize(Date src, Type typeOfSrc, JsonSerializationContext
+                context) {
+            return src == null ? null : new JsonPrimitive(src.getTime());
+        }
+    };
 }
